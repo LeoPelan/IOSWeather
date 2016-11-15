ca7fd1aab17b9d68f2b8eaf00c609cc5b44ea9c6
LeoPelan <leo.pelan@gmail.com>
2016-11-15 14:25:12 +0100

    first commit

---
+++ beginApp/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  beginApp
+//
+//  Created by Léo Pelan on 26/10/2016.
+//  Copyright © 2016 baptiste chene. All rights reserved.
+//
+
+import Foundation
---
+++ beginApp/RootWeatherCell.swift
@@ -0,0 +1,9 @@
+//
+//  RootWeatherCell.swift
+//  beginApp
+//
+//  Created by Léo Pelan on 02/11/2016.
+//  Copyright © 2016 baptiste chene. All rights reserved.
+//
+
+import Foundation
